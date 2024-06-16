d5f24442cee124b1c2813ea622fae557b5a779fa
Igor de Lima Mendes <32309693+igorlimam@users.noreply.github.com>
2024-06-16 18:31:46 -0300

    Update SetAllAgesToX.lua

--- SetAllAgesToX.lua
+++ SetAllAgesToX.lua
@@ -2,7 +2,7 @@
 -- FIFA's Career mode goes on for 15 years, by the 8th, most famous players are regen or almost retiring.
 -- To add insult to injury, most regens come with very similar names (I had over 40 players called "Branco")
 -- and many share alike appearances
--- For that matter, this script removes retiring condition and set all birthdates to a certain time.
+-- For that matter, this script removes current retiring condition and set all birthdates to a certain time.
 
 local rows = GetDBTableRows("players")
 
